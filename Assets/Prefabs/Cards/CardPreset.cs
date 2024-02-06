9758a0ff67ec264304ee9232e935840a639302a2
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-02-06 23:56:56 +0100

    android build support update

--- Assets/Prefabs/Cards/CardPreset.cs
+++ Assets/Prefabs/Cards/CardPreset.cs
@@ -1,9 +1,5 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Presets;
 using UnityEngine;
 using UnityEngine.UI;
 
